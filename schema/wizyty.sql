ec57ce4d89638179650934605b03cb21e00fdc3d
KarMa2880 <marckarm2880@gmail.com>
2026-03-27 22:52:32 +0100

    update wizyty

--- schema/wizyty.sql
+++ schema/wizyty.sql
@@ -1,1 +1,11 @@
 
+
+CREATE TABLE `wizyty` (
+  `id_wizyty` int NOT NULL AUTO_INCREMENT,
+  `id_klienta` int DEFAULT NULL,
+  `data_wizyty` date DEFAULT NULL,
+  `opis` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_wizyty`),
+  KEY `id_klienta` (`id_klienta`),
+  CONSTRAINT `wizyty_ibfk_1` FOREIGN KEY (`id_klienta`) REFERENCES `klienci` (`id_klienta`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
